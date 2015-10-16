83b70ceafbfd22ebe10e969b88c35070f48acebc
Zuzah <murtaza.hasni@gmail.com>
2015-10-16 01:01:41 -0400

    Added new libraries: HighCharts and .less minification. Changed .css to .less. Created new Branch tab, placed hardcoded PieChart.

---
+++ client/templates/branches/branchChart.js
@@ -0,0 +1,44 @@
+//chart injected into the customerRepsIndex template
+
+Template.branchIndex.helpers({
+  topGenresChart: function() {
+    return {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: this.username + " Most Surveyed Branches"
+        },
+        tooltip: {
+            pointFormat: '<b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'genre',
+            data: [
+                ['Bay/Dundas',   45.0],
+                ['Commerce Court',       26.8],
+                ['Queens Street',   12.8],
+                ['Ranee Ave',    8.5],
+                ['Bayveiw Village',     6.2]
+            ]
+        }]
+    };
+  }
+});
